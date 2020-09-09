94959d464f191e11f8920216b576977419a789f1
artobnje22 <70949554+artobnje22@users.noreply.github.com>
2020-09-09 17:54:26 +0530

    made the change division

--- code1.cs
+++ code1.cs
@@ -13,14 +13,18 @@ namespace add
             int Number1, Number2;  
             Console.WriteLine("please enter the Number1");  
             Number1 = Convert.ToInt32(Console.ReadLine());  
-            Console.WriteLine("please enter the Number2");  
-            Number2 = Convert.ToInt32(Console.ReadLine());  
+            Console.WriteLine("please enter the Number2");
+            Number2 = Convert.ToInt32(Console.ReadLine());
             int Sum;
             int Product;
+            int div=-999;
             Sum = Number1 + Number2; 
             Product = Number1*Number2;
+            if(Number2!=0)
+                div = Number1 / Number2;
             Console.WriteLine("Sum of two Numbers:" + Sum.ToString()); 
-            Console.WriteLine("Product of two Numbers:" + Product.ToString());  
+            Console.WriteLine("Product of two Numbers:" + Product.ToString());
+            Console.WriteLine("Division of two Numbers:" + div.ToString());
             Console.ReadLine();  
         }  
     }  
